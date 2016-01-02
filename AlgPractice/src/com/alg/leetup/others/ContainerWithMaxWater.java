6eaaa3caf2c8ebd68342150d95ce73210da20c3f
Ramesh Baral <rameshrajbaral@yahoo.com>
2016-01-02 12:48:08 -0500

    file name changed for container with most water

--- AlgPractice/src/com/alg/leetup/others/ContainerWithMaxWater.java
+++ AlgPractice/src/com/alg/leetup/others/ContainerWithMaxWater.java
@@ -1,10 +1,15 @@
+/**
+ * Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+Note: You may not slant the container.
+ */
 package com.alg.leetup.others;
 /**
  * ref:http://rafal.io/posts/leetcode-11-container-with-most-water.html
  * @author rbaral
  *
  */
-public class BucketWithMaxWater {
+public class ContainerWithMaxWater {
     public int maxArea(int[] height) {
         int maxA = 0;
         int i = 0;
