38e3727589ee97d2974d6491492b1691bfd15643
Ramesh Baral <rameshrajbaral@yahoo.com>
2016-01-02 13:06:26 -0500

    file description updated

--- AlgPractice/src/com/alg/leetup/others/MedianOfSortedArray.java
+++ AlgPractice/src/com/alg/leetup/others/MedianOfSortedArray.java
@@ -1,3 +1,9 @@
+/**
+There are two sorted arrays nums1 and nums2 of size m and n respectively. 
+Find the median of the two sorted arrays. 
+The overall run time complexity should be O(log (m+n)).
+*/
+
 package com.alg.leetup.others;
 
 import java.util.Arrays;
